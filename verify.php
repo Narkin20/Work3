2de53e404d8a844fb5ce96dc7bbb4764e053b470
Narkin20 <kimanuga@gmail.com>
2022-09-01 14:38:10 +0700

    sec

---
+++ verify.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <h1><center>Webboard narkin</center></h1>
+    <hr>
+    <div>
+    <center>เข้าสู่ระบบด้วย<br>
+    Login = <?php echo $_POST["login"]; ?><br>
+    Password = <?php echo $_POST["pass"]; ?><br></center>
+    </div>
+    
+</body>
+</html>
